440613956dc0293cdb6c925b793bff7cf38d86c5
rattle-venom <66112630+rattle-venom@users.noreply.github.com>
2020-05-29 21:12:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var thehours = new Date().getHours();
+	var themessage;
+	var morning = ('Selamat Pagi Rizka :3');
+	var afternoon = ('Selamat Siang dedek emeush:3');
+	var evening = ('Selamat Malam bebz :3');
+	var afternoon2 = ('Selamat Sore Zheyenk')
+	var sleep = ('Selamat Tidur Rizka Sayang')
+
+	if (thehours >= 0 && thehours < 12) {
+		themessage = morning; 
+
+	} else if (thehours >= 12 && thehours < 15) {
+		themessage = afternoon;
+
+	} else if (thehours >= 18 && thehours < 20) {
+		themessage = evening;
+
+	} else if (thehours >= 15 && thehours < 18) {
+		themessage = afternoon2;
+
+	} else if (thehours >= 20 && thehours < 24) {
+		themessage = sleep;
+	
+	}
+
+	$('.greeting').append(themessage);
